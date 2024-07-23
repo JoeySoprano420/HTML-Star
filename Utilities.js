1243f69b42dc2765a26237d2e48faef47f798c29
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-07-23 19:31:47 -0400

    Create Utilities.js

---
+++ Utilities.js
@@ -0,0 +1,8 @@
+const utils = {
+  isNumber: (value) => typeof value === 'number',
+  isString: (value) => typeof value === 'string',
+  isBoolean: (value) => typeof value === 'boolean',
+  // ... Additional utility functions ...
+};
+
+module.exports = utils;
